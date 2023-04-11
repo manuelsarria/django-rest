3bf0d2622b3f9ddc095565dbccea59821fea7d77
Manuel Sarrria <victor.sarria@utp.ac.pa>
2023-04-11 11:25:05 -0500

    task with crontab beat and celery

--- backend/src/src/celery.py
+++ backend/src/src/celery.py
@@ -1,8 +1,31 @@
 import os
 from celery import Celery
 
+from django.core.mail import send_mail
+from celery.schedules import crontab
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'src.settings.base')
 
 app = Celery('src')
+
 app.config_from_object('django.conf:settings', namespace='CELERY')
+
 app.autodiscover_tasks()
+
+@app.on_after_configure.connect
+def setup_periodic_tasks(sender, **kwargs):
+    # ...
+    sender.add_periodic_task(
+        crontab(hour=7, minute=30, day_of_week=1),
+        send_my_email.s('subject', 'message', 'from@example.com', ['to@example.com']),
+    )
+
+@app.task
+def send_my_email(subject, message, fromEmail, recipients):
+    send_mail(
+        subject,
+        message,
+        fromEmail,
+        recipients,
+        fail_silently=False,
+    )
--- backend/src/src/run_celery.sh
+++ backend/src/src/run_celery.sh
@@ -1,10 +1,5 @@
-#!/bin/sh
-
-# wait for redis server to start
-sleep 10
-
 # run Celery worker for our project myproject with Celery configuration stored in Celeryconf
-su -m deploy -c "celery -A src.celery worker -Q default -n default@%h"
+su -m deploy -c "celery -A src.celery worker -Q default -n default@%h && celery -A src.celery beat -l info"
 
 sleep 5
 
