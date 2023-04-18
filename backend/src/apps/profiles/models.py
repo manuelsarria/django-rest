9ae17b4af5e78c84e3c6151f8366b4bb58d56071
Manuel Sarrria <victor.sarria@utp.ac.pa>
2023-04-18 08:15:55 -0500

    add signals

--- backend/src/apps/profiles/models.py
+++ backend/src/apps/profiles/models.py
@@ -73,10 +73,12 @@ class Profile(models.Model):
     def __str__(self):
         return self.user.first_name
     
-    @receiver(post_save, sender=User)
-    def create_user_profile(sender, instance, created, **kwargs):
-      if created:
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
         Profile.objects.create(user=instance)
+        
+post_save.connect(create_user_profile, sender=Profile)
         
 # class UserProfiles(models.Model):
 #     user = models.ForeignKey(User, on_delete=models.CASCADE)
